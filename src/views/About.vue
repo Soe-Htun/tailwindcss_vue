1e99b5e411f4ae1d0775fdedda1af3144ea75026
Soehtun <soehtuncu@gmail.com>
2020-08-06 15:52:40 +0630

    first tailwindcss

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,45 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
-  </div>
+    <div>
+        <ul class="flex inline-block justify-between">
+            <li class="ml-2">
+                <a class="text-black-500 text-xl focus:text-blue-500">Active</a>
+            </li>
+            <li >
+                <a class="text-black-500 text-xl focus:text-blue-500">Link</a>
+            </li>
+            <li >
+                <a class="text-black-500 text-xl focus:text-blue-500" >Link</a>
+            </li>
+            <li >
+                <a class="text-gray-500 text-xl cursor-not-allowed" >Disabled</a>
+            </li>
+            <li >
+                <a class="float-right mr-2 text-xl focus:text-blue-500 cursor-pointer" @click="logout()">logout</a>
+            </li>
+        </ul>
+        <br>
+        <h2 class="font-bold text-5xl mt-10">Hello, welcome from About page</h2>
+
+        <div class="flex items-center bg-gray-200 h-24">
+            <div class="flex-1 text-gray-700 text-center bg-gray-400 px-4 py-2 m-2">1</div>
+            <div class="flex-1 text-gray-700 text-center bg-gray-400 px-4 py-2 m-2">2</div>
+            <div class="flex-1 text-gray-700 text-center bg-gray-400 px-4 py-2 m-2">3</div>
+        </div>
+    </div>
 </template>
+
+<script>
+export default {
+    name:'app',
+    data() {
+        return {
+            
+        }
+    },
+    methods:{
+        logout(){
+            this.$router.replace('/')
+        }
+    }
+}
+</script>>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,46 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+<div>
+  <div class="bg-gray-200 py-4">
+    <span class="block text-gray-700 bg-gray-400 px-4 py-2">1</span>
+    <span class="block text-gray-700 text-center bg-gray-400 px-4 py-2 mt-2">2</span>
+    <span class="block text-gray-700 text-center bg-gray-400 px-4 py-2 mt-2">3</span>
   </div>
+
+  <div class="bg-gray-200">
+    <div class="inline-block text-gray-700 text-center bg-gray-400 px-4 py-2 m-2">1</div>
+    <div class="inline-block text-gray-700 text-center bg-gray-400 px-4 py-2 m-2">2</div>
+    <div class="inline-block text-gray-700 text-center bg-gray-400 px-4 py-2 m-2">3</div>
+  </div><br>
+  
+
+  <input type="text" v-model="name" placeholder="Enter name" class="border border-gray-500 focus:border-blue-400 px-6 py-2 rounded-full outline-none mt-20" /><br><br>
+  <input v-model="password" placeholder="Enter password" class=" border border-gray-500  focus:border-blue-400 py-2 px-6 rounded-full outline-none"> <br><br>
+  <button class=" bg-blue-500 text-gray-200 px-6 py-2 rounded-full focus:bg-blue-700" @click="login()">Login</button>
+  <button class=" bg-blue-500 text-gray-200 px-6 py-2 ml-5 rounded-full" @click="clear()">Clear</button><br><br>
+
+</div>
+ 
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
+  name: 'Nav',
+  data() {
+    return {
+      name:'',
+      password:''
+    }
+  },
+  methods:{
+    login(){
+      this.$router.replace({name:"about"})
+    },
+    clear(){
+      this.name ='';
+      this.password =''
+    }
   }
 }
 </script>
+
+
